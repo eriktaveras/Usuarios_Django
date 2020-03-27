5c31cc6e93db8cb5eb063018f727e37bc46870ab
Erik Taveras <eriktaveras@gmail.com>
2020-03-26 21:32:54 -0400

    arreglando el view

--- users/views.py
+++ users/views.py
@@ -24,25 +24,17 @@ def register(request):
     return render(request, "users/register.html", {'form': form})
 
 def login(request):
-    # Creamos el formulario de autenticación vacío
     form = AuthenticationForm()
     if request.method == "POST":
-        # Añadimos los datos recibidos al formulario
         form = AuthenticationForm(data=request.POST)
-        # Si el formulario es válido...
         if form.is_valid():
-            # Recuperamos las credenciales validadas
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
 
-            # Verificamos las credenciales del usuario
             user = authenticate(username=username, password=password)
 
-            # Si existe un usuario con ese nombre y contraseña
             if user is not None:
-                # Hacemos el login manualmente
                 do_login(request, user)
-                # Y le redireccionamos a la portada
                 return redirect('/')
     return render(request, "users/login.html", {'form' : form})
 
