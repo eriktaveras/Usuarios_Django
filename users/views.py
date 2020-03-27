cdd75594abaa263fa26ae60173bdf35a3c5ba574
Erik Taveras <eriktaveras@gmail.com>
2020-03-26 21:30:13 -0400

    agregando sistema de usuarios

---
+++ users/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout as do_logout
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as do_login
+from django.contrib.auth.forms import UserCreationForm
+
+def welcome(request):
+    # Si estamos identificados devolvemos la portada
+    if request.user.is_authenticated:
+        return render(request, "users/welcome.html")
+    # En otro caso redireccionamos al login
+    return redirect('/login')
+
+def register(request):
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            if user is not None:
+                do_login(request, user)
+                return redirect('/')
+    return render(request, "users/register.html", {'form': form})
+
+def login(request):
+    # Creamos el formulario de autenticación vacío
+    form = AuthenticationForm()
+    if request.method == "POST":
+        # Añadimos los datos recibidos al formulario
+        form = AuthenticationForm(data=request.POST)
+        # Si el formulario es válido...
+        if form.is_valid():
+            # Recuperamos las credenciales validadas
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            # Verificamos las credenciales del usuario
+            user = authenticate(username=username, password=password)
+
+            # Si existe un usuario con ese nombre y contraseña
+            if user is not None:
+                # Hacemos el login manualmente
+                do_login(request, user)
+                # Y le redireccionamos a la portada
+                return redirect('/')
+    return render(request, "users/login.html", {'form' : form})
+
+def logout(request):
+    do_logout(request)
+    return redirect('/')
+
+
